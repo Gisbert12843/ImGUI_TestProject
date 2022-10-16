cc7ac35fa27a77ababa6287570a047fed9808337
Kai Oestreich <51728041+Gisbert12843@users.noreply.github.com>
2022-10-16 12:59:03 +0200

    working SmartButtons

--- frontend.cpp
+++ frontend.cpp
@@ -9,39 +9,25 @@
 		//static float value = 0.5f;
 		//ImGui::DragFloat("value", &value);
 		//ImGui::Text("(% .1f FPS)", 1000.0 / double(ImGui::GetIO().Framerate), double(ImGui::GetIO().Framerate));
+		char* x = (char*) vectoroffunctions.size();
+		//ImGui::Text((x));
 		
-		
-		
-		/*
-		ImGui::SetCursorPos(ImVec2(0,0));
-		ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(192, 192, 192, 192));
-		ImGuiHoveredFlags hoverflag = 0;
-		if (ImGui::Button("Einkauf", ImVec2(ImGui::GetIO().DisplaySize.x / 5, ImGui::GetIO().DisplaySize.y * 0.14)), hoverflag) {
-			vectoroffunctions.clear();
-			vectoroffunctions.push_back(EinkaufGUI);
-		}
-		if (hoverflag)
-		{
-			ImGui::PopStyleColor();
-			ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(192, 192, 192, 192));
-			ImGui::PopStyleColor();
-		}
-		else
-		ImGui::PopStyleColor();
-		*/
+	
 		static const auto size = ImVec2(ImGui::GetIO().DisplaySize.x / 5, ImGui::GetIO().DisplaySize.y * 0.14);
 		static const ImVec4 hoverColor{ 0.60f, 0.60, 0.60, 1.0f };
 		static const ImVec4 chosenColor{ 0.5f, 0, 0, 1.0f };
 		static const ImVec4 normalColor{ 0.69f, 0.69, 0.69, 1.0f };
-		static ImVec4 currentColor = normalColor;
+		static ImVec4 currentColor_b0 = normalColor;
+		static ImVec4 currentColor_b1 = normalColor;
+		static ImVec4 currentColor_b2 = normalColor;
+		static ImVec4 currentColor_b3 = normalColor;
 
 
+		ImGui::PushStyleColor(ImGuiCol_Button, currentColor_b0);
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, currentColor_b0);
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, currentColor_b0);
 		ImGui::SetCursorPos(ImVec2(0, 0));
-		ImGui::PushStyleColor(ImGuiCol_Button, currentColor);
-		ImGui::PushStyleColor(ImGuiCol_ButtonActive, currentColor);
-		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, currentColor);
 		auto state_b0 = SmartButton("Einkauf", size);
-		ImGui::PopStyleColor(3);
 		if (state_b0 == SmartButtonState::Pressed)
 		{
 			vectoroffunctions.clear();
@@ -49,29 +35,22 @@
 		}
 		if (vectoroffunctions.size() > 0 && vectoroffunctions.at(0) == EinkaufGUI)
 		{
-			currentColor = chosenColor;
+			currentColor_b0 = chosenColor;
 		}
 		else if (state_b0 == SmartButtonState::Hovered)
-			currentColor = hoverColor;
+			currentColor_b0 = hoverColor;
 		else
-			currentColor = normalColor;
-
-
-
-
-		//ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(192, 192, 192, 192));
-		//ImGui::SetCursorPos(ImVec2(ImGui::GetIO().DisplaySize.x / 5, 0));
-		//if (ImGui::Button("Verkauf", ImVec2(ImGui::GetIO().DisplaySize.x / 5, ImGui::GetIO().DisplaySize.y * 0.14))) {
-		//	vectoroffunctions.clear();
-		//	vectoroffunctions.push_back(VerkaufGUI);
-		//}
-		//ImGui::PopStyleColor();
-		ImGui::SetCursorPos(ImVec2(ImGui::GetIO().DisplaySize.x / 5, 0));
-		ImGui::PushStyleColor(ImGuiCol_Button, currentColor);
-		ImGui::PushStyleColor(ImGuiCol_ButtonActive, currentColor);
-		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, currentColor);
-		auto state_b1 = SmartButton("Verkauf", size);
+			currentColor_b0 = normalColor;
 		ImGui::PopStyleColor(3);
+
+
+
+
+		ImGui::PushStyleColor(ImGuiCol_Button, currentColor_b1);
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, currentColor_b1);
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, currentColor_b1);
+		ImGui::SetCursorPos(ImVec2(ImGui::GetIO().DisplaySize.x / 5, 0));
+		auto state_b1 = SmartButton("Verkauf", size);
 		if (state_b1 == SmartButtonState::Pressed)
 		{
 			vectoroffunctions.clear();
@@ -79,69 +58,56 @@
 		}
 		if (vectoroffunctions.size() > 0 && vectoroffunctions.at(0) == VerkaufGUI)
 		{
-			currentColor = chosenColor;
+			currentColor_b1 = chosenColor;
 		}
 		else if (state_b1 == SmartButtonState::Hovered)
-			currentColor = hoverColor;
+			currentColor_b1 = hoverColor;
 		else
-			currentColor = normalColor;
+			currentColor_b1 = normalColor;
+		ImGui::PopStyleColor(3);
 
 
 
-		//ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(192, 192, 192, 192));
 		//ImGui::SetCursorPos(ImVec2(ImGui::GetIO().DisplaySize.x / 5 * 2, 0));
-		//if (ImGui::Button("Lager", ImVec2(ImGui::GetIO().DisplaySize.x / 5, ImGui::GetIO().DisplaySize.y * 0.14))) {
+		//ImGui::PushStyleColor(ImGuiCol_Button, currentColor);
+		//ImGui::PushStyleColor(ImGuiCol_ButtonActive, currentColor);
+		//ImGui::PushStyleColor(ImGuiCol_ButtonHovered, currentColor);
+		//auto state_b2 = SmartButton("Lager", size);
+		//ImGui::PopStyleColor(3);
+		//if (state_b2 == SmartButtonState::Pressed)
+		//{
 		//	vectoroffunctions.clear();
 		//	vectoroffunctions.push_back(LagerGUI);
 		//}
-		//ImGui::PopStyleColor();
-		ImGui::SetCursorPos(ImVec2(ImGui::GetIO().DisplaySize.x / 5 * 2, 0));
-		ImGui::PushStyleColor(ImGuiCol_Button, currentColor);
-		ImGui::PushStyleColor(ImGuiCol_ButtonActive, currentColor);
-		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, currentColor);
-		auto state_b2 = SmartButton("Lager", size);
-		ImGui::PopStyleColor(3);
-		if (state_b2 == SmartButtonState::Pressed)
-		{
-			vectoroffunctions.clear();
-			vectoroffunctions.push_back(LagerGUI);
-		}
-		if (vectoroffunctions.size() > 0 && vectoroffunctions.at(0) == LagerGUI)
-		{
-			currentColor = chosenColor;
-		}
-		else if (state_b2 == SmartButtonState::Hovered)
-			currentColor = hoverColor;
-		else
-			currentColor = normalColor;
+		//if (vectoroffunctions.size() > 0 && vectoroffunctions.at(0) == LagerGUI)
+		//{
+		//	currentColor = chosenColor;
+		//}
+		//else if (state_b2 == SmartButtonState::Hovered)
+		//	currentColor = hoverColor;
+		//else
+		//	currentColor = normalColor;
 
 
-		//ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(192, 192, 192, 192));
 		//ImGui::SetCursorPos(ImVec2(ImGui::GetIO().DisplaySize.x / 5 * 3, 0));
-		//if (ImGui::Button("Lieferanden", ImVec2(ImGui::GetIO().DisplaySize.x / 5, ImGui::GetIO().DisplaySize.y * 0.14))) {
+		//ImGui::PushStyleColor(ImGuiCol_Button, currentColor);
+		//ImGui::PushStyleColor(ImGuiCol_ButtonActive, currentColor);
+		//ImGui::PushStyleColor(ImGuiCol_ButtonHovered, currentColor);
+		//auto state_b3 = SmartButton("Lierferanden", size);
+		//ImGui::PopStyleColor(3);
+		//if (state_b3 == SmartButtonState::Pressed)
+		//{
 		//	vectoroffunctions.clear();
 		//	vectoroffunctions.push_back(LieferantenGUI);
 		//}
-		//ImGui::PopStyleColor();
-		ImGui::SetCursorPos(ImVec2(ImGui::GetIO().DisplaySize.x / 5 * 3, 0));
-		ImGui::PushStyleColor(ImGuiCol_Button, currentColor);
-		ImGui::PushStyleColor(ImGuiCol_ButtonActive, currentColor);
-		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, currentColor);
-		auto state_b3 = SmartButton("Lierferanden", size);
-		ImGui::PopStyleColor(3);
-		if (state_b3 == SmartButtonState::Pressed)
-		{
-			vectoroffunctions.clear();
-			vectoroffunctions.push_back(LieferantenGUI);
-		}
-		if (vectoroffunctions.size() > 0 && vectoroffunctions.at(0) == LieferantenGUI)
-		{
-			currentColor = chosenColor;
-		}
-		else if (state_b3 == SmartButtonState::Hovered)
-			currentColor = hoverColor;
-		else
-			currentColor = normalColor;
+		//if (vectoroffunctions.size() > 0 && vectoroffunctions.at(0) == LieferantenGUI)
+		//{
+		//	currentColor = chosenColor;
+		//}
+		//else if (state_b3 == SmartButtonState::Hovered)
+		//	currentColor = hoverColor;
+		//else
+		//	currentColor = normalColor;
 
 
 		ImGui::End();
